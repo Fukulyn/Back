a4ed6067cdcc94c247574a3fbcfdf083daedce10
Spencer <a098838628@gmail.com>
2024-12-17 17:51:27 +0800

    Up

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -4,50 +4,55 @@ import { UserService } from "../Service/UserService";
 import { resp } from "../utils/resp";
 import { DBResp } from "../interfaces/DBResp";
 import { Student } from "../interfaces/Student";
-require('dotenv').config()
+require("dotenv").config();
 
 export class UserController extends Contorller {
-    protected service: UserService;
+  protected service: UserService;
 
-    constructor() {
-        super();
-        this.service = new UserService();
+  constructor() {
+    super();
+    this.service = new UserService();
+  }
+
+  public async findAll(Request: Request, Response: Response) {
+    const res: resp<Array<DBResp<Student>> | undefined> = {
+      code: 200,
+      message: "",
+      body: undefined,
+    };
+
+    const dbResp = await this.service.getAllStudents();
+    if (dbResp) {
+      res.body = dbResp;
+      res.message = "find sucess";
+      Response.send(res);
+    } else {
+      res.code = 500;
+      res.message = "server error";
+      Response.status(500).send(res);
     }
+  }
 
-    public async findAll(Request: Request, Response: Response) {
+  public async insertOne(Request: Request, Response: Response) {
+    const resp = await this.service.insertOne(Request.body);
+    Response.status(resp.code).send(resp);
+  }
 
-        const res: resp<Array<DBResp<Student>> | undefined> = {
-            code: 200,
-            message: "",
-            body: undefined
-        }
+  public async deleteById(Request: Request, Response: Response) {
+    const resp = await this.service.deleteById(Request.query.id as string);
+    Response.status(resp.code).send(resp);
+  }
 
-        const dbResp = await this.service.getAllStudents();
-        if (dbResp) {
-            res.body = dbResp;
-            res.message = "find sucess";
-            Response.send(res);
-        } else {
-            res.code = 500;
-            res.message = "server error";
-            Response.status(500).send(res);
-        }
+  public async updateNameByID(Request: Request, Response: Response) {
+    const resp = await this.service.updateNameByID(
+      Request.body.id,
+      Request.body.name
+    );
+    Response.status(resp.code).send(resp);
+  }
 
-    }
-
-    public async insertOne(Request: Request, Response: Response) {
-        const resp = await this.service.insertOne(Request.body)
-        Response.status(resp.code).send(resp)
-    }
-
-    public async deleteById(Request: Request, Response: Response) {
-        const resp = await this.service.deleteById(Request.query.id as string)
-        Response.status(resp.code).send(resp)
-    }
-    
-    public async updateNameByID(Request: Request, Response: Response) {
-        const resp = await this.service.updateNameByID(Request.body.id,Request.body.name)
-        Response.status(resp.code).send(resp)
-    }
-
+  public async findById(Request: Request, Response: Response) {
+    const resp = await this.service.findById(Request.query.id as string);
+    Response.status(resp.code).send(resp);
+  }
 }
